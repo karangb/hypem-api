33a36d78ed6cd5202c4662d894af95b0d3298e63
Karan G <karangb@gmail.com>
2013-11-02 02:08:06 +0000

    Added mp3
    eg.
    
    http://localhost:4567/mp3?hypemId=1z53b

--- app.rb
+++ app.rb
@@ -8,3 +8,8 @@ get "/obsessed" do
   user = Hypem.user(params[:username])
   user.obsessed_playlist.get.to_json
 end
+
+get "/mp3" do
+  content_type :json
+  Hypem::TrackMp3.new(params[:hypemId]).get.to_json
+end
